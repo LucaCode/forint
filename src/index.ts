29c01b1318dbef3fe622cb05a9b9292ec2ee2e04
Luca Scaringella <lucalstyle@gmail.com>
2019-09-01 17:43:13 +0200

    Added library exports.

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+/*
+Author: Luca Scaringella
+GitHub: LucaCode
+Copyright(c) Luca Scaringella
+ */
+
+import buildQueryFunc from "./lib/core";
+export {ForintQuery}  from "./lib/types";
+const forint = buildQueryFunc;
+export default forint;
