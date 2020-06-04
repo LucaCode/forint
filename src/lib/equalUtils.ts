788e7fef46338af556211ea1b9df3acd2d25af29
Luca Scaringella <lucalstyle@gmail.com>
2020-06-04 21:38:53 +0200

    Fix deepEqual and contentEqual not fully work with null.

--- src/lib/equalUtils.ts
+++ src/lib/equalUtils.ts
@@ -9,10 +9,10 @@ Copyright(c) Luca Scaringella
  * @param v1
  * @param v2
  */
-export function deepEqual(v1 : any,v2 : any) : boolean {
+export function deepEqual(v1 : any, v2 : any) : boolean {
     if (v1 === v2) return true;
-    if(typeof v1 === "object"){
-        if(typeof v2 === "object"){
+    if(typeof v1 === "object" && v1){
+        if(typeof v2 === "object" && v2){
             if(Array.isArray(v1) && Array.isArray(v2)){
                 for(let i = 0; i < v1.length; i++) if(!deepEqual(v1[i],v2[i])) return false;
                 return true;
@@ -35,10 +35,10 @@ export function deepEqual(v1 : any,v2 : any) : boolean {
  * @param v1
  * @param v2
  */
-export function contentEqual(v1 : any,v2 : any) : boolean {
+export function contentEqual(v1 : any, v2 : any) : boolean {
     if (v1 == v2) return true;
-    if(typeof v1 === "object"){
-        if(typeof v2 === "object"){
+    if(typeof v1 === "object" && v1){
+        if(typeof v2 === "object" && v2){
             if(Array.isArray(v1) && Array.isArray(v2)){
                 for(let i = 0; i < v1.length; i++) if(!contentEqual(v1[i],v2[i])) return false;
                 return true;
