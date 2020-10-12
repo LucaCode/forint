c8f9ac306d87aaadba3fe0d61347c58ac8e0aa70
Luca Scaringella <lucalstyle@gmail.com>
2020-10-12 23:36:53 +0200

    Improve dynamic test title.

--- test/src/equalUtils.test.ts
+++ test/src/equalUtils.test.ts
@@ -29,8 +29,8 @@ describe('EqualUtilsTests',() => {
             {value1: {a: '', f: {c: '12',d: 12}}, value2: {a: [], f: {c: '12',d: 12}}, expected: false},
             {value1: {a: [23,1231,{fo: 'a'}]}, value2: {a: [23,1231,{fo: 'a'}]}, expected: true},
             {value1: {a: [23,1231,{fo: 'b'}]}, value2: {a: [23,1231,{fo: 'a'}]}, expected: false},
-        ] as {value1: any, value2: any, expected: boolean}[]).forEach(({value1,value2,expected}) => {
-            it(`Should be ${!expected ? 'not ' : ''}deep equal`, () => {
+        ] as {value1: any, value2: any, expected: boolean}[]).forEach(({value1,value2,expected}, index) => {
+            it(`${index} should be ${!expected ? 'not ' : ''}deep equal`, () => {
                 assert(expected === deepEqual(value1,value2));
             });
         });
@@ -59,8 +59,8 @@ describe('EqualUtilsTests',() => {
             {value1: {a: '', f: {c: '12',d: '12'}}, value2: {a: [], f: {c: '12',d: 12}}, expected: true},
             {value1: {a: [23,1231,{fo: 'a'}]}, value2: {a: [23,1231,{fo: 'a'}]}, expected: true},
             {value1: {a: [23,1231,{fo: 'b'}]}, value2: {a: [23,1231,{fo: 'a'}]}, expected: false},
-        ] as {value1: any, value2: any, expected: boolean}[]).forEach(({value1,value2,expected}) => {
-            it(`Should be ${!expected ? 'not ' : ''}deep equal`, () => {
+        ] as {value1: any, value2: any, expected: boolean}[]).forEach(({value1,value2,expected}, index) => {
+            it(`${index} should be ${!expected ? 'not ' : ''}deep equal`, () => {
                 assert(expected === contentDeepEqual(value1,value2));
             });
         });
